d77fe98ab7031bc15f392a6c45ddfa2482ae9d22
BuyCommodity <buycommodity0@gmail.com>
2024-11-20 15:25:49 +0530

    Update page.tsx

--- buycom/app/(authenticated)/user/page.tsx
+++ buycom/app/(authenticated)/user/page.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react'
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { useRouter } from 'next/navigation'
-
+import API_URL from '@/config'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 import {
     Select,
